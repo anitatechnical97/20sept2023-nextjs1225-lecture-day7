54bc7386682f0b1c06558985851a0aa28fff9b34
anitatechnical97 <anitatechnical97@gmail.com>
2023-10-03 20:16:44 +0530

    comment2

---
+++ pages/helper/constants.js
@@ -0,0 +1,20 @@
+// 1.    Import Area
+
+
+
+
+
+// 2.    Definition Area
+
+const TITLE = 'Find Businesses Near You on Local Search Engine - Justdial';
+
+
+// 3.    Export Area
+/*
+
+    There are two ways to export elements in Javascript.
+    3.1 ->  Default Export
+    3.2 ->  Named Export
+
+*/
+export default TITLE;
---
+++ pages/helper/constants2.js
@@ -0,0 +1,22 @@
+// 1.    Import Area
+
+
+
+
+
+// 2.    Definition Area
+
+const DESCRIPTION = `Justdial, India's No. 1 local search engine, for Restaurants, Hotels, Salons, Real Estate, Travel, Healthcare, Education, B2B Businesses and more. Find addresses, phone numbers, reviews and ratings, photos, maps of businesses.`;
+
+
+
+// 3.    Export Area
+/*
+
+    There are two ways to export elements in Javascript.
+    3.1 ->  Default Export
+    3.2 ->  Named Export
+
+*/
+
+module.exports = { DESCRIPTION }
